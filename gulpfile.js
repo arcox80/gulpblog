84a45e4777dd51d81f7de9d0d10e0b241c93b7e8
Andrew Cox <drew.r.cox@gmail.com>
2016-05-24 19:01:53 -0400

    basic blog with serving

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,32 @@
-var gulp = require('gulp');
-var data = require('gulp-data');
-var sequence = require('gulp-sequence');
-var through2 = require('through2');
-var markdown = require('gulp-markdown');
-var frontMatter = require('gulp-front-matter');
+var gulp           = require('gulp');
+var data           = require('gulp-data');
+var sequence       = require('gulp-sequence');
+var through2       = require('through2');
+var markdown       = require('gulp-markdown');
+var frontMatter    = require('gulp-front-matter');
 var nunjucksRender = require('gulp-nunjucks-render');
+var bSync          = require('browser-sync');
+var reload         = bSync.reload;
 
 var site = {};
 
 var collectPosts = function() {
   var posts = [];
 
-  return through2.obj(function(file, enc, cb) {
+  var buildPost = function(file, enc, next) {
     var post = file.page;
+    post.body = file.contents.toString();
     posts.push(post);
     this.push(file);
-    cb();
-  },
-  function(cb) {
+    next();
+  };
+
+  var addPostsToSite = function(done) {
     site.posts = posts;
-    cb();
-  });
+    done();
+  }
+
+  return through2.obj(buildPost, addPostsToSite);
 };
 
 gulp.task('posts', function() {
@@ -28,7 +34,8 @@ gulp.task('posts', function() {
              .pipe(frontMatter({property: 'page', remove: true}))
              .pipe(collectPosts())
              .pipe(markdown())
-             .pipe(gulp.dest('dist/blog'));
+             .pipe(gulp.dest('dist/blog'))
+             .pipe(reload({stream: true}));
 });
 
 gulp.task('pages', function() {
@@ -37,7 +44,16 @@ gulp.task('pages', function() {
              .pipe(nunjucksRender({
                path: ['src/templates']
              }))
-             .pipe(gulp.dest('dist'));
+             .pipe(gulp.dest('dist'))
+             .pipe(reload({stream: true}));
 });
 
-gulp.task('default', sequence('posts', 'pages'));
+gulp.task('serve', function(cb) {
+  sequence('sync', cb);
+});
+
+gulp.task('sync', function() {
+  return bSync({server: { baseDir: 'dist' }});
+});
+
+gulp.task('default', sequence('posts', 'pages', 'serve'));
---
+++ src/posts/third.md
@@ -0,0 +1,15 @@
+---
+author: Andrew
+date: 2016/05/17
+title: What to Learn Post
+---
+
+## List of things I want to learn
+
+For example
+
+For instance
+
+- Javascript
+- CSS
+- Markdown
